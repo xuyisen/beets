94c48f60ddb2a036004e0c45a908599544737ce5
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-23 16:41:27 -0400

    xuyisen__diff__id_16

--- beetsplug/edit.py
+++ beetsplug/edit.py
@@ -23,7 +23,7 @@ from tempfile import NamedTemporaryFile
 
 import yaml
 
-from beets import plugins, ui, util, config
+from beets import config, plugins, ui, util
 from beets.dbcore import types
 from beets.importer import action
 from beets.ui.commands import PromptChoice, _do_query
